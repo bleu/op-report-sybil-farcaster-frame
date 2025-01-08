4f74bfd869d93af2121ad7dfa47cdcd76bcc8b00
Jean Neiverth <jeanneiverth@gmail.com>
2025-01-08 13:44:07 -0300

    chore: remove debug console at addFrame button

--- src/components/Frontend.tsx
+++ src/components/Frontend.tsx
@@ -299,7 +299,6 @@ export default function Frontend(
               >
                 Add frame to client
               </Button>
-              <span>{addFrameResult}</span>
             </div>
 
             <Button
