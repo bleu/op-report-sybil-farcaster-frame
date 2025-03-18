63ba4ffe0c33acb5ef87bfc82c452b7f0a757dfd
Jean Neiverth <jeanneiverth@gmail.com>
2025-03-18 14:14:02 -0300

    chore:refactor images

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,4 +1,6 @@
 import { ImageResponse } from "next/og";
+import fs from "fs/promises";
+import path from "path";
 
 export const alt = "Check Sybil";
 export const size = {
@@ -9,14 +11,22 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Image() {
+  // Get the path to your image
+  const imagePath = path.join(
+    process.cwd(),
+    "public",
+    "report-sybil-cover.png"
+  );
+
+  const imageBuffer = await fs.readFile(imagePath);
+  const base64Image = `data:image/png;base64,${imageBuffer.toString("base64")}`;
+
   return new ImageResponse(
     (
-      <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
-        <h1 tw="text-6xl">Check Sybil</h1>
+      <div tw="h-full w-full flex items-center justify-center">
+        <img src={base64Image} width={600} height={400} />
       </div>
     ),
-    {
-      ...size,
-    }
+    size
   );
 }
